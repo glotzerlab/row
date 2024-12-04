10d03085cfd5d7b07c3a58b6599136d519f8f4f1
Joshua A. Anderson <joaander@umich.edu>
2024-12-04 10:57:00 -0500

    Autocomplete --cluster.

--- src/cli.rs
+++ src/cli.rs
@@ -12,6 +12,7 @@ pub mod status;
 pub mod submit;
 
 use clap::{Args, Parser, Subcommand, ValueEnum};
+use clap_complete::ArgValueCandidates;
 use clap_verbosity_flag::{Verbosity, WarnLevel};
 use log::trace;
 use std::io;
@@ -51,7 +52,8 @@ pub struct GlobalOptions {
     /// Check the job submission status on the given cluster.
     ///
     /// Autodetected by default.
-    #[arg(long, global = true, env = "ROW_CLUSTER", display_order = 2)]
+    #[arg(long, global = true, env = "ROW_CLUSTER", display_order = 2,
+        add=ArgValueCandidates::new(autocomplete::get_cluster_candidates))]
     cluster: Option<String>,
 }
 
--- src/cli/autocomplete.rs
+++ src/cli/autocomplete.rs
@@ -2,6 +2,7 @@
 // Part of row, released under the BSD 3-Clause License.
 
 use clap_complete::CompletionCandidate;
+use row::cluster;
 use row::workflow::Workflow;
 
 /// List the actions in the current workflow.
@@ -14,5 +15,18 @@ pub fn get_action_candidates() -> Vec<CompletionCandidate> {
         .action
         .into_iter()
         .map(|a| CompletionCandidate::new(a.name()))
+        .collect::<Vec<_>>()
+}
+
+/// List the clusters in the user's configuration
+pub fn get_cluster_candidates() -> Vec<CompletionCandidate> {
+    let Ok(clusters) = cluster::Configuration::open() else {
+        return Vec::new();
+    };
+
+    clusters
+        .cluster
+        .into_iter()
+        .map(|a| CompletionCandidate::new(a.name))
         .collect::<Vec<_>>()
 }
